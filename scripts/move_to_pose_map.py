b3a687936cc85d74d69417ce11de66ecba6b3133
Abhishek <patilnabhi@gmail.com>
2016-02-13 19:02:49 -0600

    eband planner + move_to_pose implemented

---
+++ scripts/move_to_pose_map.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
+
+class GoToPose():
+    def __init__(self):
+        rospy.init_node('nav_test', anonymous=False)
+
+        #what to do if shut down (e.g. ctrl + C or failure)
+        rospy.on_shutdown(self.shutdown)
+
+        
+        #tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("waiting for the action server to come up...")
+        #allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+
+        #we'll send a goal to the robot to tell it to move to a pose that's near the docking station
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+
+        # Goal 1
+        goal.target_pose.pose = Pose(Point(-12.1, -4.5, 0.000), Quaternion(0.000, 0.000, 0.9, 0.4))
+        #start moving
+        self.move_base.send_goal(goal)
+        #allow TurtleBot up to 120 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(120)) 
+
+        # Goal 2
+        goal.target_pose.pose = Pose(Point(-12.3, 0.26, 0.000), Quaternion(0.000, 0.000, 0.6, 0.8))        
+        #start moving
+        self.move_base.send_goal(goal)
+        #allow TurtleBot up to 120 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(120)) 
+
+        # Goal 3
+        goal.target_pose.pose = Pose(Point(1.888, -3.2, 0.000), Quaternion(0.000, 0.000, -0.74, 0.68))        
+        #start moving
+        self.move_base.send_goal(goal)
+        #allow TurtleBot up to 120 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(120)) 
+
+
+        if not success:
+            self.move_base.cancel_goal()
+            rospy.loginfo("The base failed to reach the desired pose :(")
+        else:
+            # We made it!
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                rospy.loginfo("Hooray, reached the desired pose :)")
+
+    # def move_to_pose(self, p1, p2, q3, q4):
+    #     #we'll send a goal to the robot to tell it to move to a pose that's near the docking station
+    #     goal = MoveBaseGoal()
+    #     goal.target_pose.header.frame_id = 'map'
+    #     goal.target_pose.header.stamp = rospy.Time.now()
+    #     #customize the following Point() values so they are appropriate for your location
+    #     goal.target_pose.pose = Pose(Point(p1, p2, 0.000), Quaternion(0.000, 0.000, q3, q4))
+
+    def shutdown(self):
+        rospy.loginfo("Stopped")
+
+
+if __name__ == '__main__':
+    try:
+        GoToPose()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Exception thrown")
