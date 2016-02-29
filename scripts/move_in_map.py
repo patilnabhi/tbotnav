0784fcfec4c19806a75443c0e15053df4c77b5a9
Abhishek <patilnabhi@gmail.com>
2016-02-29 11:23:30 -0600

    updated

--- scripts/move_in_map.py
+++ scripts/move_in_map.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import Image
 import rospy
 from move_to_pose_map import GoToPose
 from std_msgs.msg import Int32
-from ar_track_alvar.msg import AlvarMarkers
+from ar_track_alvar_msgs.msg import AlvarMarkers
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
@@ -28,21 +28,31 @@ class MoveTbot:
         self.goal_x = []
         self.goal_y = []
 
-        self.qr_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, qr_callback)
-        self.odom_sub = rospy.Subscriber('odom', Odometry, odom_callback)
-        self.num_fingers_sub = rospy.Subscriber('num_fingers', Int32, num_fingers_callback)
-        self.hand_img_sub = rospy.Subscriber('hand_img', Image, hand_img_callback)
-        self.face_img_sub = rospy.Subscriber('face_img', Image, face_img_callback)
-        self.face_name_sub = rospy.Subscriber('face_name', String, face_name_callback)
+        self.qr_data = AlvarMarkers()
+
+        self.qr_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.qr_callback)
+        self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback)
+        # self.num_fingers_sub = rospy.Subscriber('num_fingers', Int32, num_fingers_callback)
+        # self.hand_img_sub = rospy.Subscriber('hand_img', Image, hand_img_callback)
+        # self.face_img_sub = rospy.Subscriber('face_img', Image, face_img_callback)
+        # self.face_name_sub = rospy.Subscriber('face_name', String, face_name_callback)
 
         self.turn_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)        
         self.rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            print self.qr_data
+        # self.tbot_routine()
 
     def qr_callback(self, data):
-        self.qr_id = data.markers.id
-        self.qr_position = data.markers.pose.pose.position
+        # print data.markers[0].pose.pose.position.x
+        # print data.markers[1].pose.pose.position.x
+        self.qr_data = data.markers
+        self.i=0
+        
+        # self.qr_position = data.markers.pose.pose.position
 
     def odom_callback(self, data):
+        # print data
         self.odom_orient = data.pose.pose.orientation
 
     def num_fingers_callback(self, data):
@@ -69,21 +79,26 @@ class MoveTbot:
     def face_name_callback(self, data):
         self.face_name = data.data
 
-    def rotate_tbot(self, th_0):
-        th = th_0
-        while (th - th_0) < np.pi/2:            
-            (_, _, th) = euler_from_quaternion([odom_data.x, odom_data.y, odom_data.z, odom_data.w])
-            if th_0 > np.pi/2:
-                th_0 = np.pi - th_0
-                if th < 0:
-                    th = -th
-                else:
-                    th = np.pi - th
+    def rotate_tbot(self, th_0):        
+        # th = th_0
+        
+        while self.i < 100:            
+            # (_, _, th) = euler_from_quaternion([self.odom_orient.x, self.odom_orient.y, self.odom_orient.z, self.odom_orient.w])
+            # if th_0 > np.pi/2:
+            #     th_0 = np.pi - th_0
+            #     if th < 0:
+            #         th = -th
+            #     else:
+            #         th = np.pi - th
             
             self.turn.linear.x = 0.0
-            self.turn.angular.z = 0.8
+            self.turn.angular.z = 0.4
 
-            self.turn_pub.publish(turn)
+            self.turn_pub.publish(self.turn)
+            # print th - th_0
+            print self.i
+            self.i += 1
+
             self.rate.sleep()
 
     def determine_goal_loc(self, proceed):
@@ -117,48 +132,51 @@ class MoveTbot:
 
     # def find_person(self):
 
-    def tbot_routine(self):
-        for i in range(4):
-            (_, _, th_0) = euler_from_quaternion([self.odom_orient.x, odom_orient.y, odom_orient.z, odom_orient.w])
-            self.rotate_tbot(th_0)
+    def tbot_routine(self):       
+        # for i in range(4):
+        (_, _, th_0) = euler_from_quaternion([self.odom_orient.x, self.odom_orient.y, self.odom_orient.z, self.odom_orient.w])
+        self.rotate_tbot(th_0)
 
-        while not rospy.is_shutdown():
-            proceed = raw_input("Press 'm' to move robot: ")
-            self.determine_goal_loc(proceed)
+        # while not rospy.is_shutdown():
+            
+        #     proceed = raw_input("Press 'm' to move robot: ")
+        #     self.determine_goal_loc(proceed)
 
-            # Search for goal location
-            for i in range(8):
-                (_, _, th_0) = euler_from_quaternion([self.odom_orient.x, odom_orient.y, odom_orient.z, odom_orient.w])
-                self.rotate_tbot(th_0)
+        #     # Search for goal location
+        #     for i in range(8):
+        #         (_, _, th_0) = euler_from_quaternion([self.odom_orient.x, odom_orient.y, odom_orient.z, odom_orient.w])
+        #         self.rotate_tbot(th_0)
                 
-                # QR detection
-                found = 0       
-                self.find_qr_tags()
+        #         # QR detection
+        #         found = 0       
+        #         self.find_qr_tags()
 
-                for j in range(len(self.qr_pos)):
-                    if self.qr_pos[j] == self.goal_loc:                    
-                        found = 1
-                        goal_idx = j
-                    else:
-                        found = 0
-                if found:
-                    break
+        #         for j in range(len(self.qr_pos)):
+        #             if self.qr_pos[j] == self.goal_loc:                    
+        #                 found = 1
+        #                 goal_idx = j
+        #             else:
+        #                 found = 0
+        #         if found:
+        #             break
 
-            self.move.move_to_pose(self.qr_pos[goal_idx])
+        #     self.move.move_to_pose(self.qr_pos[goal_idx])
 
-            self.find_person()
+        #     self.find_person()
         
 
 
     def _shutdown(self):
         rospy.loginfo("Shutting down node...")
 
-
+def test():
+    print qr_data
 
 if __name__ == '__main__':
     try:
         MoveTbot()
-        rospy.spin()
+        # test()
+        # rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("Exception thrown")
 
